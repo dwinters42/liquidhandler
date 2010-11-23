ff0c395e06bc000a39493a1224e6d1afa486ecdf
dgruber@ed.ac.uk <dgruber@ed.ac.uk>
2010-11-23 14:25:03 +0000

    add distutils support, rename RobotController.py -> RobotController

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/bin/env python
+
+# Copyright (c) 2010 Daniel Gruber <daniel@tydirium.org>
+#
+# Permission to use, copy, modify, and distribute this software for
+# any purpose with or without fee is hereby granted, provided that the
+# above copyright notice and this permission notice appear in all
+# copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+# WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+# AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+# DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA
+# OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+# TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+# PERFORMANCE OF THIS SOFTWARE.
+
+from distutils.core import setup
+
+setup(name='py-robot',
+      version="0.1",
+      license="BSD",
+      description='Control program and pythion interface to the robot',
+      author='Daniel Gruber',
+      author_email="daniel@tydirium.org",
+      package_dir={'robot': ''},
+      packages=['robot'],
+      scripts=['RobotController'],
+      classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+        ])
